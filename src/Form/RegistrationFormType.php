c3cb824e8aecf5f782ef3353afe94bc7f903611b
Dimitri21 <71702427+Dimitri21@users.noreply.github.com>
2021-03-25 17:02:58 +0100

    Reset password

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,12 +17,14 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('firstName')
+            ->add('lastName')
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => "Merci d'accepter les conditions générales d'utilisations et la politique de confidentialité",
                     ]),
                 ],
             ])
